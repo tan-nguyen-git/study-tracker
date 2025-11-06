882f1c96f8dccc693814a16ba168ea25c22523a3
tan-nguyen-git <thanhtandanang15@gmail.com>
2025-11-06 14:48:43 -0600

    pause/resume button

--- js/focus.js
+++ js/focus.js
@@ -7,6 +7,9 @@ let numSec =0; // starting time
 let second = document.getElementById("display");
 let mainButton = document.getElementById("start-button");
 second.textContent = numSec;
+let signal = false;
+
+
 
 //you need to add something that stop the user to click the button
 //multiple time
@@ -15,7 +18,7 @@ second.textContent = numSec;
 //need to change the start to resume if it already run
 //every time hit the button startCounting is called 
 let id = 0;
-function startCounting(){
+function startCounting(signal){
     if(mainButton.textContent ==="Finish"){
         //what finish do? counting from start
         //change the button to start again
@@ -23,17 +26,46 @@ function startCounting(){
         numSec =0;
         second.textContent = numSec;
         mainButton.textContent ="Start";
+        //remove the pause/resume btn
+        const toRemove = document.getElementById("pause-resume-btn");
+        if(toRemove) toRemove.remove();
+
     }
-    else if(id ==0 && mainButton.textContent ==="Start"){
+    else if((id ==0 && mainButton.textContent ==="Start") || signal ){
         id = setInterval(()=>{ //this is to stop etc
             numSec++;
             second.textContent = numSec;
         },1000); //delay is 1000ms = 1sec.
-        mainButton.textContent = "Finish";
+        // if(signal == false){
+            mainButton.textContent = "Finish";
+            //add resume and stop button ....
+            const btn = document.createElement('button');
+            btn.textContent = "Pause";
+            btn.setAttribute('id','pause-resume-btn');
+            const container = document.getElementById("timer");
+            container.appendChild(btn);
+            btn.setAttribute('onclick', 'pauseReBtn()');
+        // }
+
     }
    
 }
-//add resume and stop button ....
+//function for the pauseReBtn
+
+function pauseReBtn(){
+    btn = document.getElementById("pause-resume-btn");
+    if(btn.textContent ==="Pause"){
+        btn.textContent ="Resume";
+        //call the stopCoungting function
+        stopCounting();
+    }
+    else if(btn.textContent ==="Resume"){
+        btn.textContent ="Pause";
+        signal = true;
+        startCounting(signal); //bug here ...
+    }
+
+}
 
 // this function stop the counting and set id = 0
 function stopCounting(){
